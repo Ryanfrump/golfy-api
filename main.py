bf393ed899f456bba4c3601845f82900ad626271
Ryanfrump <ryanfrump@gmail.com>
2024-04-04 10:38:07 -0600

    add @app.get

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import uuid
+
 from fastapi import FastAPI
+
+from models.player import Player
+from models.course import Course
 
 
 app = FastAPI()
+
+players: dict[uuid.UUID, Player] = {}
+courses: dict[uuid.UUID, Course] = {}
+
+@app.get("/player")
+
+#@app.post("/player")
+
+#@app.put("/player/{player_id}")
+
+#@app.delete("/player/{player_id}")
+
+
+#@app.get("/course")
+
+#@app.post("/course")
+
+#@app.put("/course/{course_id}")
+
+#@app.delete("/course{course_id}")
