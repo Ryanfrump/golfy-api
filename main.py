4936ca5364362aeedabcf95366e39eb018dad57e
Ryanfrump <ryanfrump@gmail.com>
2024-04-08 07:51:33 -0600

    finsh adding put and delete

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import uuid
 
 from fastapi import FastAPI
 
-from models.player import Player
+from models.player import Player, CreatePlayerRequest, PlayerResponse
 from models.course import Course
 
 
@@ -12,12 +12,25 @@ players: dict[uuid.UUID, Player] = {}
 courses: dict[uuid.UUID, Course] = {}
 
 @app.get("/player")
+async def get_players() -> list[Player]:
+    return players.values()
 
-#@app.post("/player")
+@app.post("/player")
+async def create_player(player_detail: CreatePlayerRequest) -> uuid.UUID:
+    player_id = uuid.uuid4()
+    player = Player(id=player_id, name=player_detail.name, handicap=player_detail.handicap)
+    players[player.id] = player
+    return player.id
 
-#@app.put("/player/{player_id}")
+@app.put("/player/{player_id}")
+async def update_player(player_id: uuid.UUID, new_player: CreatePlayerRequest):
+    updated_player = Player(id=player_id, name=new_player.name, handicap=new_player.handicap)
+    players[player_id] = updated_player
 
-#@app.delete("/player/{player_id}")
+
+@app.delete("/player/{player_id}")
+async def delete_player(player_id: uuid.UUID):
+    del players[player_id]
 
 
 #@app.get("/course")
--- models/player.py
+++ models/player.py
@@ -7,3 +7,12 @@ class Player(BaseModel):
     id: UUID
     name: str
     handicap: float
+
+
+class CreatePlayerRequest(BaseModel):
+    name: str
+    handicap: float
+
+
+class PlayerResponse(BaseModel):
+    id: UUID
